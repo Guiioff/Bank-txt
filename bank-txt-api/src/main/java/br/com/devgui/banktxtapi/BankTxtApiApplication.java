70c0a1961d72c4bb9ecb7d9073a12f5baad9994f
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-04-06 16:26:06 -0300

    feat: inicializa a estrutura do projeto da api

---
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/BankTxtApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.devgui.banktxtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTxtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTxtApiApplication.class, args);
+    }
+
+}
---
+++ bank-txt-api/src/test/java/br/com/devgui/banktxtapi/BankTxtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devgui.banktxtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTxtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
