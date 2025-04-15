44e62f9f9ba15d80b6383f79a1f44efa15cc599d
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-15 11:58:12 -0300

    feat: cria a interface e implementação do service de usuario

---
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/UsuarioService.java
@@ -0,0 +1,8 @@
+package br.com.devgui.banktxtapi.service;
+
+import br.com.devgui.banktxtapi.model.Usuario;
+
+public interface UsuarioService {
+
+    void cadastrarUsuario(Usuario usuario);
+}
---
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,14 @@
+package br.com.devgui.banktxtapi.service.impl;
+
+import br.com.devgui.banktxtapi.model.Usuario;
+import br.com.devgui.banktxtapi.service.UsuarioService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Override
+    public void cadastrarUsuario(Usuario usuario) {
+
+    }
+}
