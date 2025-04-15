4262d31120e3d4dafe15efca73c0d58bdc8e1b30
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-15 11:50:05 -0300

    feat: cria controller de Transacao

---
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/controller/TransacaoController.java
@@ -0,0 +1,23 @@
+package br.com.devgui.banktxtapi.controller;
+
+import br.com.devgui.banktxtapi.service.TransacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/transacoes")
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void upload(@RequestParam("arquivo") MultipartFile arquivo, @RequestParam("usuarioId") Long usuarioId) {
+        transacaoService.processarArquivo(arquivo, usuarioId);
+    }
+}
