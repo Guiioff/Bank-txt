d16ad7843ad4109809e73f3f64903b6dcba1e8c8
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-15 12:05:05 -0300

    feat: implementa método para cadastrar usuário

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
@@ -1,14 +1,21 @@
 package br.com.devgui.banktxtapi.service.impl;
 
 import br.com.devgui.banktxtapi.model.Usuario;
+import br.com.devgui.banktxtapi.repository.UsuarioRepository;
 import br.com.devgui.banktxtapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioServiceImpl implements UsuarioService {
 
-    @Override
-    public void cadastrarUsuario(Usuario usuario) {
+    private UsuarioRepository usuarioRepository;
 
+    @Override
+    @Transactional
+    public void cadastrarUsuario(Usuario usuario) {
+        usuarioRepository.save(usuario);
     }
 }
