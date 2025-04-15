196bf3b138585b82905a90abcf21926c876b04c5
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-15 14:46:07 -0300

    feat: adição de novos métodos naa interface de Transacao

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/TransacaoService.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/TransacaoService.java
@@ -1,11 +1,16 @@
 package br.com.devgui.banktxtapi.service;
 
 import br.com.devgui.banktxtapi.model.Transacao;
+import br.com.devgui.banktxtapi.model.Usuario;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface TransacaoService {
 
-    List<Transacao> processarArquivo(MultipartFile arquivo, Long usuarioId);
+    void processarTransacoes(MultipartFile arquivo, Long usuarioId);
+
+    List<Transacao> lerArquivo(MultipartFile arquivo, Usuario usuario);
+
+    void salvarTransacoes(List<Transacao> transacoes, Usuario usuario);
 }
