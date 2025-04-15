3db4e91526ae6e8c7527791bb6cd3b69d5cbc266
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-15 12:26:34 -0300

    feat: adiciona validação nos dados de cadastro de usuário

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/controller/request/UsuarioCadastroRequestDTO.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/controller/request/UsuarioCadastroRequestDTO.java
@@ -1,10 +1,24 @@
 package br.com.devgui.banktxtapi.controller.request;
 
 import br.com.devgui.banktxtapi.model.Usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record UsuarioCadastroRequestDTO(String nome,
-                                        String email,
-                                        String senha) {
+public record UsuarioCadastroRequestDTO(
+
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+        String nome,
+
+        @NotBlank (message = "O e-mail é obrigatório")
+        @Email(message = "E-mail inválido")
+        @Size(max = 150, message = "O e-mail deve ter no máximo 150 caracteres")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, max = 255, message = "A senha deve ter entre 6 e 255 caracteres")
+        String senha) {
 
     public Usuario toEntity() {
         return new Usuario(nome, email, senha);
