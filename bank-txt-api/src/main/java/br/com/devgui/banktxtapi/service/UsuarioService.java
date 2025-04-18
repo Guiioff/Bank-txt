21843d5a57d8e98f70baae51ff29f6eb21aff719
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-17 23:31:39 -0300

    feat: define método na interface para listar transações de um usuário

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/UsuarioService.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package br.com.devgui.banktxtapi.service;
 
+import br.com.devgui.banktxtapi.controller.response.UsuarioTransacoesResponseDTO;
 import br.com.devgui.banktxtapi.model.Usuario;
 
 public interface UsuarioService {
 
     void cadastrarUsuario(Usuario usuario);
+
+    UsuarioTransacoesResponseDTO listarTransacoes(Long idUsuario);
 }
