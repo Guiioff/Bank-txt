e976e7002a8ed7c439924166c8f032f99e988dc2
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-17 23:32:35 -0300

    feat: cria o método para retornar transações do usuário de acordo com o ID no repository

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/repository/TransacaoRepository.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/repository/TransacaoRepository.java
@@ -3,4 +3,9 @@ package br.com.devgui.banktxtapi.repository;
 import br.com.devgui.banktxtapi.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransacaoRepository extends JpaRepository<Transacao, Long> {}
+import java.util.List;
+
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+    List<Transacao> findByUsuarioId(Long id);
+}
