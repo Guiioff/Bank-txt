04d158f3289558e6df2e2ee992fd573d81cd8268
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-17 23:33:24 -0300

    feat: implementa método para retornar lista de transações de um usuário

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
@@ -1,8 +1,13 @@
 package br.com.devgui.banktxtapi.service.impl;
 
+import br.com.devgui.banktxtapi.controller.response.TransacaoResponseDTO;
+import br.com.devgui.banktxtapi.controller.response.UsuarioTransacoesResponseDTO;
 import br.com.devgui.banktxtapi.model.Usuario;
+import br.com.devgui.banktxtapi.repository.TransacaoRepository;
 import br.com.devgui.banktxtapi.repository.UsuarioRepository;
 import br.com.devgui.banktxtapi.service.UsuarioService;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +15,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class UsuarioServiceImpl implements UsuarioService {
 
     private UsuarioRepository usuarioRepository;
+    private TransacaoRepository transacaoRepository;
 
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository, TransacaoRepository transacaoRepository) {
         this.usuarioRepository = usuarioRepository;
+        this.transacaoRepository = transacaoRepository;
     }
 
     @Override
     @Transactional
     public void cadastrarUsuario(Usuario usuario) {
         usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UsuarioTransacoesResponseDTO listarTransacoes(Long idUsuario) {
+        Usuario usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com o ID: " + idUsuario));
+
+        List<TransacaoResponseDTO> transacaoResponseDTOS = transacaoRepository
+                .findByUsuarioId(usuario.getId())
+                .stream()
+                .map(t -> new TransacaoResponseDTO(
+                        t.getTipo(),
+                        t.getValor(),
+                        t.getData()))
+                .toList();
+
+        return new UsuarioTransacoesResponseDTO(usuario.getNome(), usuario.getEmail(), transacaoResponseDTOS);
     }
 }
