caf99d97eb651e3a6e81142461dde2a23af2e80b
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-19 19:29:35 -0300

    feat: define os métodos para retornar a primeira e última transação de um usuário

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/repository/TransacaoRepository.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/repository/TransacaoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
@@ -19,4 +20,16 @@ public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
     List<Transacao> buscarEntreDatas(@Param("usuarioId") Long usuarioId,
                                      @Param("dataInicial") LocalDate dataInicial,
                                      @Param("dataFinal") LocalDate dataFinal);
+
+    @Query("SELECT t FROM Transacao t WHERE " +
+            "t.usuario.id = :usuarioId " +
+            "ORDER BY t.data DESC " +
+            "LIMIT 1")
+    Optional<Transacao> buscarUltimaTransacao(@Param("usuarioId") Long usuarioId);
+
+    @Query("SELECT t FROM Transacao t WHERE " +
+            "t.usuario.id = :usuarioId " +
+            "ORDER BY t.data ASC " +
+            "LIMIT 1")
+    Optional<Transacao> buscarPrimeiraTransacao(@Param("usuarioId") Long usuarioId);
 }
