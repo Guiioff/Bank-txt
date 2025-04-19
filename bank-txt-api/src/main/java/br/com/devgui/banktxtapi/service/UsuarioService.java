3c2df7b8259a179abe3d0a87c25fcdf05cd3bef2
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-19 17:55:51 -0300

    feat: define o método para listar as retornar as transações de um usuário entre datas na interface do service do Usuário

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/UsuarioService.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/UsuarioService.java
@@ -3,9 +3,13 @@ package br.com.devgui.banktxtapi.service;
 import br.com.devgui.banktxtapi.controller.response.UsuarioTransacoesResponseDTO;
 import br.com.devgui.banktxtapi.model.Usuario;
 
+import java.time.LocalDate;
+
 public interface UsuarioService {
 
     void cadastrarUsuario(Usuario usuario);
 
     UsuarioTransacoesResponseDTO listarTransacoes(Long idUsuario);
+
+    UsuarioTransacoesResponseDTO listarTransacoesEntreDatas(Long idUsuario, LocalDate dataInicial, LocalDate dataFinal);
 }
