be131c696ddcc9c02ac5f3c1b0833b2e29ebe7aa
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-19 17:55:00 -0300

    feat: define método para retornar transações de um usuário entre datas

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/repository/TransacaoRepository.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/repository/TransacaoRepository.java
@@ -1,11 +1,22 @@
 package br.com.devgui.banktxtapi.repository;
 
 import br.com.devgui.banktxtapi.model.Transacao;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
     List<Transacao> findByUsuarioId(Long id);
+
+    @Query("SELECT t FROM Transacao t WHERE " +
+            "t.usuario.id = :usuarioId AND " +
+            "t.data BETWEEN :dataInicial AND :dataFinal")
+    List<Transacao> buscarEntreDatas(@Param("usuarioId") Long usuarioId,
+                                     @Param("dataInicial") LocalDate dataInicial,
+                                     @Param("dataFinal") LocalDate dataFinal);
 }
