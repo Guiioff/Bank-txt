284185430469b3d3da589dcec884beef5e9390a9
Guiioff <guilhermerivaldy288@hotmail.com>
2025-04-19 17:56:51 -0300

    feat: implementa método para listar as transações de um usuário entre datas

--- bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
+++ bank-txt-api/src/main/java/br/com/devgui/banktxtapi/service/impl/UsuarioServiceImpl.java
@@ -2,11 +2,14 @@ package br.com.devgui.banktxtapi.service.impl;
 
 import br.com.devgui.banktxtapi.controller.response.TransacaoResponseDTO;
 import br.com.devgui.banktxtapi.controller.response.UsuarioTransacoesResponseDTO;
+import br.com.devgui.banktxtapi.model.Transacao;
 import br.com.devgui.banktxtapi.model.Usuario;
 import br.com.devgui.banktxtapi.repository.TransacaoRepository;
 import br.com.devgui.banktxtapi.repository.UsuarioRepository;
 import br.com.devgui.banktxtapi.service.UsuarioService;
 import jakarta.persistence.EntityNotFoundException;
+
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,6 +45,21 @@ public class UsuarioServiceImpl implements UsuarioService {
                         t.getData()))
                 .toList();
 
+        return new UsuarioTransacoesResponseDTO(usuario.getNome(), usuario.getEmail(), transacaoResponseDTOS);
+    }
+
+    @Override
+    public UsuarioTransacoesResponseDTO listarTransacoesEntreDatas(Long idUsuario, LocalDate dataInicial, LocalDate dataFinal) {
+        Usuario usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com o ID: " + idUsuario));
+
+        List<Transacao> transacaos = transacaoRepository.buscarEntreDatas(usuario.getId(), dataInicial, dataFinal);
+        List<TransacaoResponseDTO> transacaoResponseDTOS = transacaos.stream()
+                .map(t -> new TransacaoResponseDTO(
+                        t.getTipo(),
+                        t.getValor(),
+                        t.getData()))
+                .toList();
         return new UsuarioTransacoesResponseDTO(usuario.getNome(), usuario.getEmail(), transacaoResponseDTOS);
     }
 }
